2eabf03ffdd7c582eb87605fae8d5fa69cff9286
MuIZI <141940146+MuizValiyev@users.noreply.github.com>
2025-06-01 15:18:23 +0500

    fix search

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -6,17 +6,15 @@ import styles from "./nav.module.css";
 import { motion, AnimatePresence } from "motion/react";
 import Cookies from "js-cookie";
 
-
 export default function Nav() {
   const [modal, setModal] = useState(false);
   const [accountOrCart, setAccountOrCart] = useState(1);
 
-    useEffect(() => {
-      if (Cookies.get("access_token")) {
-        setAccountOrCart(2);
-      }
-    }, [])
-
+  useEffect(() => {
+    if (Cookies.get("access_token")) {
+      setAccountOrCart(2);
+    }
+  }, []);
 
   useEffect(() => {
     if (modal) {
@@ -24,14 +22,17 @@ export default function Nav() {
     } else {
       document.body.style.overflow = "auto";
     }
-  }, [modal])
+  }, [modal]);
 
   return (
     <>
       <div className={styles.mainNav}>
         <div className={styles.boxNavCenter}>
           <div className={styles.boxLogo}>
-            <button onClick={() => setModal(!modal)} className={styles.burgerMenu}>
+            <button
+              onClick={() => setModal(!modal)}
+              className={styles.burgerMenu}
+            >
               <Image
                 src="/burger.svg"
                 alt="burger menu"
@@ -40,29 +41,43 @@ export default function Nav() {
               />
             </button>
             <Link href={"/"} className={styles.boxLogoLink}>
-              <Image src="/decabrinaLogo.svg" alt="logo" width={408} height={46} />
-              <Image src="/logoAdaptive.svg" alt="logo" width={144} height={20} />
+              <Image
+                src="/decabrinaLogo.svg"
+                alt="logo"
+                width={408}
+                height={46}
+              />
+              <Image
+                src="/logoAdaptive.svg"
+                alt="logo"
+                width={144}
+                height={20}
+              />
             </Link>
           </div>
-          <Link href="#" className={styles.oneLinkNav}>продукция</Link>
-          <Link href="/about" className={styles.oneLinkNav}>о предприятии</Link>
-          <Link href="/delivery" className={styles.oneLinkNav}>доставка</Link>
+          <Link href="#" className={styles.oneLinkNav}>
+            продукция
+          </Link>
+          <Link href="/about" className={styles.oneLinkNav}>
+            о предприятии
+          </Link>
+          <Link href="/delivery" className={styles.oneLinkNav}>
+            доставка
+          </Link>
           <div className={styles.boxSearch}>
             <button>RU</button>
-            <div className={styles.boxInput}>
-              <input type="seatch" placeholder="поиск" />
-              <Image src="/search.svg" alt="search" width={24} height={24} />
-            </div>
+            <>
+              {accountOrCart !== 1 ? (
+                <Link href="/cart">
+                  <Image src="/cart.svg" alt="cart" width={28} height={20} />
+                </Link>
+              ) : (
+                <Link href="/login" className={styles.boxAccount}>
+                  <Image src="/login.svg" alt="login" width={24} height={24} />
+                </Link>
+              )}
+            </>
           </div>
-          {accountOrCart !== 1 ? (
-          <Link href="/cart">
-            <Image src="/cart.svg" alt="cart" width={28} height={20} />
-          </Link>
-          ) : (
-            <Link href="/login" className={styles.boxAccount}>
-              <Image src="/login.svg" alt="login" width={24} height={24}/>
-            </Link>
-          )}
         </div>
       </div>
       <AnimatePresence>
@@ -72,18 +87,15 @@ export default function Nav() {
             animate={{ y: 0, opacity: 1, display: "flex" }}
             exit={{ y: -10, opacity: 0, display: "none" }}
             transition={{ duration: 0.1 }}
-            className={styles.modal}>
+            className={styles.modal}
+          >
             <Link href="#">продукция</Link>
             <Link href="/about">о предприятии</Link>
             <Link href="/delivery">доставка</Link>
-            <div className={styles.boxSearchAdaptive}>
-                <input type="search" placeholder="поиск" />
-                <button><Image src="/search.svg" alt="search" width={24} height={24} /></button>
-            </div>
             <div className={styles.boxRowLanguage}>
-                <button className={styles.oneLanguage}>RU</button>
-                <button className={styles.oneLanguage}>UZ</button>
-                <button className={styles.oneLanguage}>EN</button>
+              <button className={styles.oneLanguage}>RU</button>
+              <button className={styles.oneLanguage}>UZ</button>
+              <button className={styles.oneLanguage}>EN</button>
             </div>
           </motion.div>
         )}
